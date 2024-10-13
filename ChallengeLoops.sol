5f4556bd501ef237f12f3f5925f207699e3b915e
シャナ Shana <103087920+AngeloCoder27@users.noreply.github.com>
2024-10-13 11:21:50 +0800

    Update ChallengeLoops.sol

--- ChallengeLoops.sol
+++ ChallengeLoops.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.7;
 // For this challenge, do a simple operation, and run it in 1000 loops.
 
 contract ForAndWhileLoops {
-
-     // Function to calculate the sum of the first _n numbers
     function increment(uint _n) external pure returns (uint) {
         uint s; 
         for (uint i = 1; i <= _n; i++) {
